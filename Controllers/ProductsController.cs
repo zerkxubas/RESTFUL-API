e73a77e373c3fc032bd4d9113df3ffb6dcd61db0
Subash Sharma <zerkxubas@gmail.com>
2024-01-26 21:46:45 +0545

    deleting product using DELETE method

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -86,5 +86,21 @@ namespace apiproject.Controllers
             }
             return NoContent();
         }
+
+        // Deleting A Product
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NoContent();
+            }
+            
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
     }
 }
